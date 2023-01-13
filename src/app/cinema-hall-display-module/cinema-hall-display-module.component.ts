020d4f139e32b0f1e3613bc875e2b2bd807dff0c
wolfi-m <der.wolfgang.amadeus.mozart@gmx.at>
2023-01-13 18:43:17 +0100

    hall-editor resizing works

--- src/app/cinema-hall-display-module/cinema-hall-display-module.component.ts
+++ src/app/cinema-hall-display-module/cinema-hall-display-module.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,} from '@angular/core';
 import { CinemaHall, dummyCinemaHall } from '../model/cinemaHallInterface';
-import { Seat, SeatCategory } from '../model/seatInterface';
 
 
 
@@ -10,23 +9,13 @@ import { Seat, SeatCategory } from '../model/seatInterface';
   styleUrls: ['./cinema-hall-display-module.component.css']
 })
 export class CinemaHallDisplayModuleComponent {
-  // add flags or enum of states
-  // is loaded, stored in db
-  // is a new cinemaHall
-
-  hallId = -1
-  hallName = "default hall name"
-
-  rows = 10
-  cols = 10
-
   @Input() cinemaHall: CinemaHall
 
-  constructor() {
+  constructor( ) {
     this.cinemaHall = dummyCinemaHall
   }
 
-  public getCinemaHall() : CinemaHall {
+  public getCinemaHall(): CinemaHall {
     return this.cinemaHall
   }
 }
--- src/app/edit-cinema-hall/edit-cinema-hall.component.ts
+++ src/app/edit-cinema-hall/edit-cinema-hall.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { CinemaHallDisplayModuleComponent } from '../cinema-hall-display-module/cinema-hall-display-module.component';
 import { CinemaHall, dummyCinemaHall } from '../model/cinemaHallInterface';
 import { Seat, SeatCategory } from '../model/seatInterface';
@@ -14,8 +15,8 @@ export class EditCinemaHallComponent {
   hallId = -1
   hallName = "default hall name"
 
-  rows: number = 10
-  cols: number = 10
+  numRows: number = 10
+  numCols: number = 10
   seats: Seat[][] = []
 
 
@@ -23,34 +24,36 @@ export class EditCinemaHallComponent {
   // is loaded, stored in db
   // is a new cinemaHall
 
-  cinemaHall: CinemaHall = dummyCinemaHall//model
+  cinemaHall: CinemaHall = dummyCinemaHall
 
 
-  constructor() {
-    this.createHallModel()
+  constructor(
+    // public cinemaHallDisplay : CinemaHallDisplayModuleComponent
+    public readonly router : Router
+  ) {
+    this.updateHallModel()
   }
 
   updateRows(event: any) {
-    console.log(event.target.value)
-    this.rows = event.target.value
-    this.createHallModel()
+    this.numRows = event.target.value
+    this.updateHallModel()
   }
 
   updateCols(event: any) {
     console.log(event.target.value)
-    this.cols = event.target.value
-    this.createHallModel()
+    this.numCols = event.target.value
+    this.updateHallModel()
   }
 
-  createHallModel() {
-    for (let x = 0, counter = 0; x < this.rows; x++) {
+  updateHallModel() {
+    this.seats = []
+    for (let numRow = 0, counter = 0; numRow < this.numRows; numRow++) {
       let row: Seat[] = []
-      for (let y = 0; y < this.cols; y++, counter++) {
+      for (let numCol = 0; numCol < this.numCols; numCol++, counter++) {
         row.push(new Seat(this.hallId, counter, SeatCategory.Normal));
       }
       this.seats.push(row)
     }
-
     this.cinemaHall = new CinemaHall(this.hallId, this.hallName, this.seats, false, false, false)
   }
 }
