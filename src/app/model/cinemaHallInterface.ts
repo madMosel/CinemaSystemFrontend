fcd395ad13531dfea5e4fa1d645ec13e2c65ba17
wolfi-m <der.wolfgang.amadeus.mozart@gmx.at>
2023-01-13 14:04:02 +0100

    cinemaHallInterface

---
+++ src/app/model/cinemaHallInterface.ts
@@ -0,0 +1,11 @@
+export class CinemaHall {
+    constructor (
+        public hallId: number,
+        public Seat: [][],
+        public dolby: boolean,
+        public d3: boolean,
+        public d4: boolean
+    ) {
+
+    }
+}
