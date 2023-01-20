c75b2487403a7e15fc6db40ed204f92730637835
wolfi-m <der.wolfgang.amadeus.mozart@gmx.at>
2023-01-20 22:02:38 +0100

    movieList EditButton works

--- src/app/hall-list/hall-list.component.ts
+++ src/app/hall-list/hall-list.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { CinemaHall, dummyCinemaHall } from '../model/cinemaHallInterface';
 import mockCinemas from '../../assets/mockCinemas.json';
-import { Router } from '@angular/router';
-import { EditCinemaHallComponent } from '../edit-cinema-hall/edit-cinema-hall.component';
-
 @Component({
   selector: 'app-hall-list',
   templateUrl: './hall-list.component.html',
@@ -15,7 +12,6 @@ export class HallListComponent {
   hallSelected: boolean = false
 
   constructor(
-    private readonly router: Router
   ) {
     console.log(this.cinemaHalls)
   }
