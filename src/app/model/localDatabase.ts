fb0fe8b61045ecc04e0075ecda8cbae38d5388c3
wolfi-m <der.wolfgang.amadeus.mozart@gmx.at>
2023-01-29 13:34:28 +0100

    fixed memory glitches

--- src/app/model/localDatabase.ts
+++ src/app/model/localDatabase.ts
@@ -38,26 +38,52 @@ export class LocalDatabase {
     private minutesOffset: number = 15
 
     constructor() {
-        let state = localStorage.getItem("dbData")
-        if (state) {
-            console.log("update...")
-            this.cinemaHalls = JSON.parse(localStorage.getItem("dbData-halls")!) as CinemaHall[]
-            this.movies = JSON.parse(localStorage.getItem("dbData-movies")!)
-            this.schedules = parseScheduleAdaptersToSchedules(JSON.parse(localStorage.getItem("dbData-schedules")!))
-            console.log(localStorage.getItem("dbData-schedules")!)
-            console.log(JSON.parse(localStorage.getItem("dbData-schedules")!))
-            this.localUser = JSON.parse(localStorage.getItem("dbData-user")!)
-            this.tickets = JSON.parse(localStorage.getItem("dbData-tickets")!)
-        }
-        else {
-            this.cinemaHalls = mockCinemas as CinemaHall[]
-            this.movies = mockMovies as Movie[]
-            this.schedules = parseScheduleAdaptersToSchedules(mockSchedules)
-            console.log("init...")
-
-            //load all visible data from remote database
-        }
         this.createMaps()
+    }
+
+    async loadHallsFromServer() {
+        this.cinemaHalls = mockCinemas as CinemaHall[]
+    }
+
+    async loadMoviesFromServer() {
+        this.movies = mockMovies as Movie[]
+    }
+
+    async loadSchedulesFromServer() {
+        this.schedules = parseScheduleAdaptersToSchedules(mockSchedules)
+    }
+
+    async loadTicketsFromServer() {
+
+    }
+
+
+    async loadHalls() {
+        if (!localStorage.getItem("dbData")) this.loadHallsFromServer()
+        else this.cinemaHalls = JSON.parse(localStorage.getItem("dbData-halls")!) as CinemaHall[]
+        this.createHallMap()
+    }
+
+    async loadMovies() {
+        if (!localStorage.getItem("dbData")) this.loadMoviesFromServer()
+        else this.movies = JSON.parse(localStorage.getItem("dbData-movies")!)
+        this.createMovieMap()
+    }
+
+    async loadSchedules() {
+        if (!localStorage.getItem("dbData")) this.loadSchedulesFromServer()
+        else this.schedules = parseScheduleAdaptersToSchedules(JSON.parse(localStorage.getItem("dbData-schedules")!))
+    }
+
+    async loadTickets() {
+        if (!localStorage.getItem("dbData")) this.loadTicketsFromServer()
+        else this.tickets = []
+    }
+
+    async load() {
+        this.loadHalls()
+        this.loadMovies()
+        this.loadSchedules()
     }
 
 
@@ -74,48 +100,59 @@ export class LocalDatabase {
 
 
     public getHallById(hallId: number): CinemaHall | null {
+        this.loadHalls()
         for (let hall of this.cinemaHalls) if (hall.hallId === hallId) return { ...hall }
         return null;
     }
 
     public getHalls(): CinemaHall[] {
+        this.loadHalls()
         let cinemHallsCopy: CinemaHall[] = [...this.cinemaHalls]
         return cinemHallsCopy
     }
 
     public getMovieById(movieId: number): Movie | null {
+        this.loadMovies()
         for (let movie of this.movies) if (movie.movieId === movieId) return { ...movie }
         return null
     }
 
     public getMovies(): Movie[] {
+        this.loadMovies()
         let moviesCopy: Movie[] = [...this.movies]
         return moviesCopy
     }
 
     public getSchedulesOfHall(hallId: number): Schedule[] {
+        this.loadHalls()
+        this.loadSchedules()
         let hallSchedules: Schedule[] = []
         for (let schedule of this.schedules) if (schedule.hallId === hallId) hallSchedules.push({ ...schedule })
         return hallSchedules
     }
 
     public getSchedulesOfMovie(movieId: number): Schedule[] {
+        this.loadHalls()
+        this.loadSchedules()
         let movieSchedules: Schedule[] = []
         for (let schedule of this.schedules) if (schedule.movieId === movieId) movieSchedules.push({ ...schedule })
         return movieSchedules
     }
 
     public getSchedules(): Schedule[] {
+        this.loadSchedules()
         return [...this.schedules]
     }
 
     public filterSchedulesByHallId(schedules: Schedule[], hallId: number): Schedule[] {
+        this.loadSchedules()
         let filtered: Schedule[] = []
         for (let schedule of schedules) if (schedule.hallId == hallId) filtered.push(schedule)
         return filtered
     }
 
     public putHall(hall: CinemaHall): OperationFeedback {
+
         return OperationFeedback.NOT_IMPLEMENTED
     }
 
@@ -125,6 +162,7 @@ export class LocalDatabase {
      * @returns null on Succes, a copy of the conflicting Schedule on error
      */
     public putSchedule(schedule: Schedule): Schedule | null {
+        this.loadSchedules()
         let schedulesOfHall = this.filterSchedulesByHallId(this.schedules, schedule.hallId)
         let newStart = schedule.dateTime
         let newEnd: Date = new Date(newStart)
@@ -150,6 +188,7 @@ export class LocalDatabase {
             }
         }
         this.schedules.push(schedule)
+        this.updateStorage()
         return null
     }
 
@@ -166,14 +205,17 @@ export class LocalDatabase {
     }
 
     public deleteSchedule(schedule: Schedule) {
+        this.loadSchedules()
         for (let s of this.schedules) if (compareSchedules(schedule, s)) {
             this.schedules.splice(this.schedules.indexOf(s, 0), 1)
             this.updateStorage()
             return
         }
+        this.updateStorage()
     }
 
     deleteMovie(movie: Movie): OperationFeedback {
+        this.load()
         for (let schedule of this.schedules) if (schedule.movieId == movie.movieId) {
             console.log(schedule)
             return OperationFeedback.HAS_REFERING_OBJECTS  
